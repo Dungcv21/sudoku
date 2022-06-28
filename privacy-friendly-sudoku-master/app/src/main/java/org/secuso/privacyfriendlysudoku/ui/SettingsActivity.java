8da2aa8745b94d01a5f83395e0374065e1ec323d
Vector <dung.can@netlink.vn>
2022-06-28 11:11:44 +0700

    28/06

--- privacy-friendly-sudoku-master/app/src/main/java/org/secuso/privacyfriendlysudoku/ui/SettingsActivity.java
+++ privacy-friendly-sudoku-master/app/src/main/java/org/secuso/privacyfriendlysudoku/ui/SettingsActivity.java
@@ -41,29 +41,32 @@ public class SettingsActivity extends AppCompatActivity {
 
     private static void recheckNightModeProperties(SharedPreferences sharedPreferences) {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-
-            if (sharedPreferences.getBoolean("pref_dark_mode_setting", false)) {
-
-                settingsFragment.findPreference("pref_dark_mode_automatically_by_system").setEnabled(false);
-                settingsFragment.findPreference("pref_dark_mode_automatically_by_battery").setEnabled(false);
-            } else {
-                if (sharedPreferences.getBoolean("pref_dark_mode_automatically_by_battery", false) && sharedPreferences.getBoolean("pref_dark_mode_automatically_by_system", false) ) {
-                    sharedPreferences.edit().putBoolean("pref_dark_mode_automatically_by_battery", false).commit();
-                }
-
-                settingsFragment.findPreference("pref_dark_mode_automatically_by_system").setEnabled(!sharedPreferences.getBoolean("pref_dark_mode_automatically_by_battery", false));
-                settingsFragment.findPreference("pref_dark_mode_automatically_by_battery").setEnabled(!sharedPreferences.getBoolean("pref_dark_mode_automatically_by_system", false));
-            }}
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//
+//            if (sharedPreferences.getBoolean("pref_dark_mode_setting", false)) {
+//
+//                settingsFragment.findPreference("pref_dark_mode_automatically_by_system").setEnabled(false);
+//                settingsFragment.findPreference("pref_dark_mode_automatically_by_battery").setEnabled(false);
+//            } else {
+//                if (sharedPreferences.getBoolean("pref_dark_mode_automatically_by_battery", false) && sharedPreferences.getBoolean("pref_dark_mode_automatically_by_system", false) ) {
+//                    sharedPreferences.edit().putBoolean("pref_dark_mode_automatically_by_battery", false).commit();
+//                }
+//
+//                settingsFragment.findPreference("pref_dark_mode_automatically_by_system").setEnabled(!sharedPreferences.getBoolean("pref_dark_mode_automatically_by_battery", false));
+//                settingsFragment.findPreference("pref_dark_mode_automatically_by_battery").setEnabled(!sharedPreferences.getBoolean("pref_dark_mode_automatically_by_system", false));
+//            }
+//        }
 
         if (sharedPreferences.getBoolean("pref_dark_mode_setting", false )) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
-        } else if (sharedPreferences.getBoolean("pref_dark_mode_automatically_by_system", false)) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-        } else if(sharedPreferences.getBoolean("pref_dark_mode_automatically_by_battery", false)){
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
-        } else {
+        }
+//        else if (sharedPreferences.getBoolean("pref_dark_mode_automatically_by_system", false)) {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+//        } else if(sharedPreferences.getBoolean("pref_dark_mode_automatically_by_battery", false)){
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+//        }
+        else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
     }
@@ -72,7 +75,9 @@ public class SettingsActivity extends AppCompatActivity {
 
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            if (key.equals("pref_dark_mode_setting")|| key.equals("pref_dark_mode_automatically_by_system")||key.equals("pref_dark_mode_automatically_by_battery")) {
+            if (key.equals("pref_dark_mode_setting")
+//                    || key.equals("pref_dark_mode_automatically_by_system")||key.equals("pref_dark_mode_automatically_by_battery")
+            ) {
                 recheckNightModeProperties(sharedPreferences);
             }
         }
